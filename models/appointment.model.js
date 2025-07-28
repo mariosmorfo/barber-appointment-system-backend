5b05574cb3c6fe6bf7ca5f1d10fdd4cd61de00c0
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-28 21:44:19 +0300

    feat: define Appointment schema linking users and barbers

---
+++ models/appointment.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+let appointmentSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  barberId: {
+    type: Schema.Types.ObjectId,
+    ref: "Barber",
+    required: true
+  },
+  serviceName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  dateTime: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['booked', 'cancelled', 'completed'],
+    default: 'booked'
+  }
+},
+{timestamps: true}
+)
+
+module.exports = mongoose.model(appointmentSchema)
