3cd69459906afed4ca954f456562f9f13478880d
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-28 22:28:25 +0300

    fix: refactor import app

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose")
-const app = require("app")
-const port = 3000
+const mongoose = require('mongoose');
+const app = require('./app');
+const port = 3000;
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(
