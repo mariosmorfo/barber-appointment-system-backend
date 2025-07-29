767910777917173bb7885231f7a5c32a16d19a3e
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-29 14:48:43 +0300

    feat: implement CRUD endpoints for User model

---
+++ controllers/user.controller.js
@@ -0,0 +1,86 @@
+const User = require('../models/user.model')
+const bcrypt = require('bcrypt')
+
+exports.createUser = async(req, res) => {
+  console.log('Create user')
+
+  const data = req.body;
+  const saltOrRounds = 10;
+
+  let passwordHash = "";
+
+  if(data.password)
+    passwordHash = await bcrypt.hash(data.password, saltOrRounds)
+
+  const newUser = new User({
+    firstname: data.firstname,
+    lastname: data.lastname,
+    username: data.username,
+    email: data.email,
+    password: passwordHash,
+    phone: data.phone,
+    age: data.age,
+    role: data.role || 'CUSTOMER',
+    address: data.address
+  })
+
+  try{
+    const result = await newUser.save()
+    res.status(200).json({status: true, data: result})
+  }catch(err){
+    console.log('Error for creating new user', err)
+    res.status(400).json({status: false, data: err})
+  }
+}
+
+exports.findAllUsers = async(req ,res) => {
+  console.log('Find all users')
+  
+  try{
+    const result = await User.find().select('-password')
+    res.status(200).json({status: true, data: result})
+  }catch(err){
+    console.log('Error in finding all users', err)
+    res.status(400).json({status: false, data: err})
+  }
+}
+
+exports.findUserByUsername = async(req, res) => {
+  const username = req.params.username;
+  console.log('Find user with specific username')
+
+  try{
+    const result = await User.findOne({username: username})
+    res.status(200).json({status: true, data: result})
+  }catch(err){
+    console.log('Error in finding user')
+    res.status(400).json({status: false, data: err})
+  }
+}
+
+exports.deleteByUsername = async(req, res) => {
+  const username = req.params.username;
+  console.log('Delete user with username: ', username)
+
+  try{
+    const result = await User.findOneAndDelete({username: username})
+    res.status(200).json({status: true, data: result})
+  }catch(err){
+    console.log('Error in deleting user', err)
+    res.status(400).json({status: false, data: err})
+  }
+}
+
+exports.updateUserByUsername = async(req, res) => {
+  const updateUser = req.body;
+  const username = req.params.username;
+  console.log('Update user')
+
+  try{
+    const result = await User.findOneAndUpdate({username: username}, updateUser, {new: true}).select('-password')
+    res.status(200).json({status: true, data: result})
+  }catch(err){
+    console.log('Error in updating user')
+    res.status(400).json({status: false, data: err})
+  }
+}
