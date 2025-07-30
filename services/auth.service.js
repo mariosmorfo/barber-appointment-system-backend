134802bdf33b172e6f54db34c14bb556190d6073
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-30 22:23:59 +0300

    feat: add JWT generation and verification utilities

---
+++ services/auth.service.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+
+function generateAccesToken(user){
+  const payload = {
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    role: user.role
+  }
+
+  const secret = process.env.TOKEN_SECRET;
+
+  const options = {expiresIn: '2h'}
+
+  return jwt.sign(payload, secret,options)
+}
+
+function verifyAccessToken(token) {
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    console.log('Verify token', payload)
+    return { verified: true, payload };
+  } catch (err) {
+    return { verified: false, error: err.message };
+  }
+}
+
+module.exports = {generateAccesToken, verifyAccessToken}
