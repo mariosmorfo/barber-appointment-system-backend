b2a8812de3ae077440001d368b3a572446bed822
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-31 19:16:55 +0300

    feat: add appointment booking endpoints

---
+++ routes/appointment.routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router();
+const verifyToken = require('../middlewares/auth.middleware').verifyToken;
+const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
+const appointmentController = require('../controllers/appointment.controller')
+
+router.get('/', verifyToken, verifyRoles('ADMIN'), appointmentController.findAllAppointments)
+router.get('/barber/:barberId', verifyToken, appointmentController.getAppointmentByBarber)
+router.get('/user/:userId', verifyToken, appointmentController.getAppointmentByCustomer)
+router.post('/create', verifyToken, appointmentController.createAppointment)
+router.delete('/cancel/:id', verifyToken, appointmentController.cancelAppointment)
+router.patch('/:id/status', verifyToken, verifyRoles('BARBER'), appointmentController.updateAppointmentStatus)
+
+module.exports = router;
+
