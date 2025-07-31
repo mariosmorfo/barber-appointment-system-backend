a516df7a27626cfa78efd8c8babc6754704a4bab
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-07-31 19:52:37 +0300

    feat: configure Express app with global middleware and routes

--- app.js
+++ app.js
@@ -2,4 +2,23 @@ const express = require('express');
 const app = express();  
 const cors = require('cors');
 
+const user = require('./routes/user.routes')
+const auth = require('./routes/auth.routes')
+const barber = require('./routes/barber.routes')
+const appointment = require('./routes/appointment.routes');
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use(cors({
+  origin: "*"
+}))
+
+app.use('/api/auth', auth)
+app.use('/api/user', user)
+app.use('/api/barber', barber)
+app.use('/api/appointment', appointment)
+
+
 module.exports = app;
