e508c6f2624c2f9025bed1faf4c15a94a36d4b20
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-08-01 19:02:49 +0300

    feat: add barber login route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const authController = require('../controllers/auth.controller')
 
 router.post('/login', authController.login)
+router.post('/barber-login', authController.barberLogin)
 
 module.exports = router;
