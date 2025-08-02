69b52faef4a8d754c4a4520fb209d180885bc5de
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-08-02 14:25:57 +0300

    feat: wire up swagger

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const auth = require('./routes/auth.routes')
 const barber = require('./routes/barber.routes')
 const appointment = require('./routes/appointment.routes');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
+
+app.use(
+  '/api-docs', 
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument.options)  
+)
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
